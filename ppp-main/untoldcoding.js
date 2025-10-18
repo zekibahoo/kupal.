0edf3136e46da5d564666b691b764b1179e68e93
zekibahoo <ariolanaruto5@gmail.com>
2025-10-17 14:27:46 -1200

    Add files via upload

---
+++ ppp-main/untoldcoding.js
@@ -0,0 +1,35 @@
+const text =
+  "kupal good morning";
+
+const paragraph = text.split("");
+
+let i = 0;
+
+function dashOut(arr) {
+  if (i < arr.length) {
+    console.log(arr[i]);
+    document.querySelector(".textCont").textContent += arr[i];
+
+    i++;
+    console.log("The i count: " + i);
+    setTimeout(function () {
+      dashOut(arr);
+    }, interval(arr[i]));
+  }
+}
+
+function interval(letter) {
+  console.log(letter);
+  if (letter == ";" || letter == "." || letter == ",") {
+    return Math.floor(Math.random() * 500 + 500);
+  } else {
+    return Math.floor(Math.random() * 130 + 5);
+  }
+}
+
+function startFromBegin() {
+  i = 0;
+  dashOut(paragraph);
+}
+
+startFromBegin();
